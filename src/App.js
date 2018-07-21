576d8940a1f3b30b8f6c7de73fa295ae0344108f
Adam Lessen <lessena@gmail.com>
2018-07-21 14:13:59 -0700

    Hello world autocomplete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import DropdownIntroHeader from './DropdownIntroHeader';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DropdownIntroHeader />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/DropdownIntroHeader.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class DropdownIntroHeader extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          Welcome to the <code>auotcomplete-dropdown</code>.
+        </p>
+        <p className="App-intro">
+          Enter a bank/credit/investment company below to get started!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default DropdownIntroHeader;
